c686caa8cd7c62f31552d4f9c862badb802971d1
Dennis238 <dennis.238@hotmail.com>
2018-08-01 18:00:21 -0500

    Pantalla de Inicio

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,11 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
 
 import { ReservaRestauranteComponent } from './reserva-restaurante/reserva-restaurante.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 import { ReservaHotelComponent } from './reserva-hotel/reserva-hotel.component';
 import { LoginComponent } from './login/login.component';
+
 
 
 @NgModule({
@@ -15,9 +18,11 @@ import { LoginComponent } from './login/login.component';
     ReservaRestauranteComponent,
     ReservaHotelComponent,
     LoginComponent,
+    InicioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule // NgModel
   ],
   providers: [],
   bootstrap: [AppComponent]
