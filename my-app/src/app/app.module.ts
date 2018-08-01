7f8c7e7fdc53958ceb08daec8511331b41151dcc
Avill2 <andrea.villacis@epn.edu.ec>
2018-08-01 10:14:29 -0500

    creado el compoenete reserva hotel y listo el front

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ReservaHotelComponent } from './reserva-hotel/reserva-hotel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ReservaHotelComponent
   ],
   imports: [
     BrowserModule
